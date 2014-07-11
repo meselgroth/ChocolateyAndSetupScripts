1be970030cf9df0efa338eb7e8749ea8c29edafa
meselgroth <meselgroth@yahoo.com>
2014-07-11 10:01:01 +0100

    VS menu turn caps off. First.ps1

--- first.ps1
+++ first.ps1
@@ -1,4 +1,8 @@
-cinstm VisualStudio2012Professional
+cinstm VisualStudio2012Ultimate
+Set-ItemProperty -Path HKCU:\Software\Microsoft\VisualStudio\11.0\General -Name SuppressUppercaseConversion -Type DWord -Value 1
+cinstm VisualStudio2013Ultimate
+Set-ItemProperty -Path HKCU:\Software\Microsoft\VisualStudio\12.0\General -Name SuppressUppercaseConversion -Type DWord -Value 1
+
 cinst sqlce40rtw -source webpi
 cinst sqlcevstools -source webpi
 

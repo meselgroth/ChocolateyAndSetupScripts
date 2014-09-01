dea1ccac4917a3977b6d7d9f47e25dd07ea75e47
meselgroth <meselgroth@yahoo.com>
2014-09-01 09:40:10 +0100

    No cmd needed. Start installation with boxstarter.

--- setup.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-SET DIR=%~dp0%
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
-pause

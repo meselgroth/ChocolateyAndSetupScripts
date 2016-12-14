8db26f47c14cafe8bfcd98580d6e0bd39e177f7a
meselgroth <meselgroth@yahoo.com>
2016-12-14 17:33:06 +0800

    update VS to 2015 include npmtaskrunner

--- 02_VS_and_addons.ps1
+++ 02_VS_and_addons.ps1
@@ -1,9 +1,14 @@
-cinstm VisualStudio2013Ultimate
-Set-ItemProperty -Path HKCU:\Software\Microsoft\VisualStudio\12.0\General -Name SuppressUppercaseConversion -Type DWord -Value 1
+choco install visualstudio2015community
+choco install npmtaskrunner
+# web essentials
 
-cinst sqlce40rtw -source webpi
-cinst sqlcevstools -source webpi
-cinst SqlServerLocalDb
+# cinstm VisualStudio2013Ultimate
+# Set-ItemProperty -Path HKCU:\Software\Microsoft\VisualStudio\12.0\General -Name SuppressUppercaseConversion -Type DWord -Value 1
+start http://visualstudiogallery.msdn.microsoft.com/8e1b4368-4afb-467a-bc13-9650572db708
+
+# cinst sqlce40rtw -source webpi
+# cinst sqlcevstools -source webpi
+# cinst SqlServerLocalDb
 
 #VS azure extension
 

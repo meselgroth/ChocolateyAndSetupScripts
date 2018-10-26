25c4c058444ef4f0226aeb119b63375b917f8f48
meselgroth <meselgroth@yahoo.com>
2018-10-26 23:07:05 +0100

    Update 01_initialise.ps1

--- 01_initialise.ps1
+++ 01_initialise.ps1
@@ -1,14 +1,5 @@
 chocolatey feature enable -n allowGlobalConfirmation
-cinst git
-
-$progFiles86 = ${env:ProgramFiles(x86)}
-$gitShellPath = Join-Path $progFiles86 "Git\git-cheetah\git_shell_ext64.dll"	
-
-$gitCmd = "$progFiles86\Git\cmd"
- 
-
-regsvr32 /u $gitShellPath
-# http://stackoverflow.com/questions/2459763/how-do-i-remove-msysgits-right-click-menu-options
+cinst git --params /NoShellIntegration
  
 cinst notepadplusplus
 cinst visualstudiocode

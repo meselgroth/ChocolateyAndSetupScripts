5023558a018b4e5617f6c5dbf08784a80b23fe0c
meselgroth <meselgroth@yahoo.com>
2019-07-26 17:19:14 +0100

    Update 04_JS_dev.ps1

--- 04_JS_dev.ps1
+++ 04_JS_dev.ps1
@@ -4,11 +4,13 @@ npm install -g eslint #might be mac only
 
 npm install jasmine-node -g
 
-#new node proj
+#new node proj, modules requires 12.7+
 npm init
 npm install jasmine
+npm i esm
 node node_modules/jasmine/bin/jasmine init
-#package.json "scripts": { "test": "jasmine" }
+#package.json "type": "module",
+#package.json "scripts": { "test": "node -r esm node_modules/jasmine/bin/jasmine" }
 
 eslint --init
 #eslintrc.json "env": { ..., "jasmine" : true }

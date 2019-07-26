996cbc28bdaf260743fb23c799ea9d6e0370c912
meselgroth <meselgroth@yahoo.com>
2019-07-26 15:39:04 +0100

    Create 04_JS_dev.ps1

---
+++ 04_JS_dev.ps1
@@ -0,0 +1,16 @@
+#VS Code extenstions: Visual Studio Keymap, ESLint, React Pure To Class, Reactjs code snippets (weird abbr. google results showed another one to try)
+
+npm install -g eslint #might be mac only
+
+npm install jasmine-node -g
+
+#new node proj
+npm init
+npm install jasmine
+node node_modules/jasmine/bin/jasmine init #maybe not needed with jasmine-node
+#package.json "scripts": { "test": "jasmine" }
+
+eslint --init
+#eslintrc.json "env": { ..., "jasmine" : true }
+
+npm install jasmine-node -g

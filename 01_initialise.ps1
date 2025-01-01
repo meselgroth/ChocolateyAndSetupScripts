22fc757b1b0db62ba2d38105ea76025a361337d0
Michael E <meselgroth@yahoo.com>
2025-01-01 15:55:51 +1100

    Add chocolatey install

--- 01_initialise.ps1
+++ 01_initialise.ps1
@@ -1,3 +1,6 @@
+Set-ExecutionPolicy Bypass -Scope Process
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 chocolatey feature enable -n allowGlobalConfirmation
 choco install choco-shim-redirects
 cinst git --params /NoShellIntegration

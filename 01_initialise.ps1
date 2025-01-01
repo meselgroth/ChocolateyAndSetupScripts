f256c51631e45468f94a4bf8d5f70629befcb413
Michael E <meselgroth@yahoo.com>
2025-01-01 15:52:17 +1100

    Update 01_initialise.ps1

--- 01_initialise.ps1
+++ 01_initialise.ps1
@@ -2,7 +2,8 @@ chocolatey feature enable -n allowGlobalConfirmation
 choco install choco-shim-redirects
 cinst git --params /NoShellIntegration
  
-cinst notepadplusplus
+#cinst notepadplusplus
 cinst visualstudiocode
 
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
 Write-Host "Finished."
